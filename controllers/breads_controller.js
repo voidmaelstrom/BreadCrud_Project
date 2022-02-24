bce0427b322f631494a7cf6a16945f958a8d1302
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-02-23 19:52:44 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,9 +2,24 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+// // INDEX
+// breads.get('/', (req, res) => {
+//   res.render('index',
+//     {
+//       breads: Bread
+//     }
+//   )
+// // res.send(Bread)
+// })
+
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
 })
 
 // SHOW
---
+++ views/layouts/default.jsx
@@ -0,0 +1,33 @@
+const React = require('react')
+
+// function Default(html) {
+//   return (
+//     <html>
+//     <head>
+//       <title>Default</title>
+//     </head>
+//     <body>
+//       <h1>HTML Rendered!</h1>
+//       <div className="container">
+//         {html.children}
+//       </div>
+//     </body>
+//     </html>
+//   )
+// }
+
+function Default(html) {
+  return (
+    <html>
+    <head>
+      <title>{html.title || 'Default'}</title>
+    </head>
+    <body>
+      <h1>HTML Rendred!</h1>
+      {html.children}
+    </body>
+    </html>
+  )
+}
+
+module.exports = Default
