c4aa342dc5bc7a1c2d1e84c333b1d6e3e3835583
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-02-26 09:41:16 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -24,3 +25,8 @@ app.listen(PORT, () => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
