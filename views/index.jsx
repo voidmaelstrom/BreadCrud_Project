c4c668086b7cba290925e7133c0265d2dc2064b7
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-05 08:52:32 -0700

    Fix index rendering when no breads are present

--- views/index.jsx
+++ views/index.jsx
@@ -1,11 +1,10 @@
 const React = require('react')
-const Default = require('./layouts/Default')
+const Default = require('./layouts/default')
 
 function Index ({breads, title})  {
   return (
     <Default title={title}>
       <h2>Index Page</h2>
-      <p>I have {breads[0].name} bread!</p>
       <ul>
         {
           breads.map((bread, index)=> {
