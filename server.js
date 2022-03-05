df143c6569d268b2b580c2528d4883b8634136e6
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-05 09:44:01 -0700

    Add create and read with mongoose ODM implemented

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 const app = express()
 
 // MIDDLEWARE
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-// MIDDLEWARE
 app.use(methodOverride('_method'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
