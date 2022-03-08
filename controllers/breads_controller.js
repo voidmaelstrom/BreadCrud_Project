abb0cb33a3fd024d1e85bd1afdebcb0ef5943330
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-07 18:32:04 -0700

    Add a couple custom helper methods

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -54,6 +54,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
       .then(foundBread => {
+          const bakedBy = foundBread.getBakedBy()
+          console.log(bakedBy)
           res.render('show', {
               bread: foundBread
           })
