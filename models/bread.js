561399fa92509fa4f90b005c0280e362dfb348e7
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-07 17:40:07 -0700

    Add validation and support for baker field with data

--- models/bread.js
+++ models/bread.js
@@ -6,8 +6,12 @@ const { Schema } = mongoose
 // schema
 const breadSchema = new Schema({
     name: { type: String, required: true },
-    hasGluten: { type: Boolean },
-    image: { type: String, default: 'http://placehold.it/500x500.png' }
+    hasGluten: Boolean,
+    image: { type: String, default: 'http://placehold.it/500x500.png' },
+    baker: {
+      type: String,
+      enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+    }
 })
 
 // model and export 
