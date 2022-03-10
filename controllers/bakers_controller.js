eeb8c4e6a5d64103dd02731e751bc980f2bb926f
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-09 18:38:12 -0700

    Apply virtuals and apply baker to breads relationships

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -12,3 +12,23 @@ baker.get('/data/seed', (req, res) => {
   Baker.insertMany(bakerSeedData)
       .then(res.redirect('/breads'))
 })
+
+// Index: 
+baker.get('/', (req, res) => {
+  Baker.find()
+      .populate('breads')
+      .then(foundBakers => {
+          res.send(foundBakers)
+      })
+})
+
+// Show: 
+baker.get('/:id', (req, res) => {
+  Baker.findById(req.params.id)
+      .populate('breads')
+      .then(foundBaker => {
+          res.render('bakerShow', {
+              baker: foundBaker
+          })
+      })
+})
