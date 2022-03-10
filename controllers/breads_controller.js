7b4c8351a11b5d839e45e2dd41010d56d2bffbd6
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-09 18:09:25 -0700

    Scaffold the bakers model and associate to the breads

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -59,9 +59,10 @@ breads.get('/new', (req, res) => {
 // SHOW
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
+      .populate('baker')
       .then(foundBread => {
-          const bakedBy = foundBread.getBakedBy()
-          console.log(bakedBy)
+          // const bakedBy = foundBread.getBakedBy()
+          // console.log(bakedBy)
           res.render('show', {
               bread: foundBread
           })
@@ -135,11 +136,15 @@ breads.put('/:id', (req, res) => {
 
 // EDIT
 breads.get('/:id/edit', (req, res) => {
-  Bread.findById(req.params.id) 
-    .then(foundBread => { 
-      res.render('edit', {
-        bread: foundBread 
-      })
+  Baker.find()
+    .then(foundBakers => {
+      Bread.findById(req.params.id) 
+        .then(foundBread => { 
+          res.render('edit', {
+            bread: foundBread,
+            bakers: foundBakers
+          })
+        })
     })
 })
 
